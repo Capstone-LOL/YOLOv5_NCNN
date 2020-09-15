0f0599ef21b6d2911359d1c7f9c65d11daffac06
WZT <ascode@qq.com>
2020-09-15 14:17:48 +0800

    reformat code

--- android_YOLOV5_NCNN/app/src/main/cpp/YoloV5.h
+++ android_YOLOV5_NCNN/app/src/main/cpp/YoloV5.h
@@ -7,18 +7,18 @@
 
 #include "ncnn/net.h"
 
-namespace yolocv{
-    typedef struct{
+namespace yolocv {
+    typedef struct {
         int width;
         int height;
-    }YoloSize;
+    } YoloSize;
 }
 
 typedef struct {
     std::string name;
     int stride;
     std::vector<yolocv::YoloSize> anchors;
-}YoloLayerData;
+} YoloLayerData;
 
 typedef struct BoxInfo {
     float x1;
@@ -31,9 +31,11 @@ typedef struct BoxInfo {
 
 class YoloV5 {
 public:
-    YoloV5(AAssetManager* mgr, const char* param, const char* bin, bool useGPU);
+    YoloV5(AAssetManager *mgr, const char *param, const char *bin, bool useGPU);
+
     ~YoloV5();
-    std::vector<BoxInfo> detect(JNIEnv* env, jobject image, float threshold, float nms_threshold);
+
+    std::vector<BoxInfo> detect(JNIEnv *env, jobject image, float threshold, float nms_threshold);
     std::vector<std::string> labels{"person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
                                     "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
                                     "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
@@ -44,15 +46,19 @@ public:
                                     "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
                                     "hair drier", "toothbrush"};
 private:
-    static std::vector<BoxInfo> decode_infer(ncnn::Mat &data, int stride,const yolocv::YoloSize& frame_size, int net_size,int num_classes,const std::vector<yolocv::YoloSize>& anchors,float threshold);
-    static void nms(std::vector<BoxInfo>& result,float nms_threshold);
-    ncnn::Net* Net;
+    static std::vector<BoxInfo>
+    decode_infer(ncnn::Mat &data, int stride, const yolocv::YoloSize &frame_size, int net_size, int num_classes,
+                 const std::vector<yolocv::YoloSize> &anchors, float threshold);
+
+    static void nms(std::vector<BoxInfo> &result, float nms_threshold);
+
+    ncnn::Net *Net;
     int input_size = 640;
     int num_class = 80;
     std::vector<YoloLayerData> layers{
-        {"394",32,{{116,90},{156,198},{373,326}}},
-        {"375",16,{{30,61},{62,45},{59,119}}},
-        {"output",8,{{10,13},{16,30},{33,23}}},
+            {"394",    32, {{116, 90}, {156, 198}, {373, 326}}},
+            {"375",    16, {{30,  61}, {62,  45},  {59,  119}}},
+            {"output", 8,  {{10,  13}, {16,  30},  {33,  23}}},
     };
 
 public:
